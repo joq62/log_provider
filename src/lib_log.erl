7ca17de8f8ce225aecbce5fcd76a8fa736fab946
joq62 <jakim.leche@gmail.com>
2023-04-17 22:54:09 +0200

    ud

--- src/lib_log.erl
+++ src/lib_log.erl
@@ -32,6 +32,12 @@
 create_logfile(MainLogDir,ProviderLogDir,LogFile,LogFilePath,MaxLogLength)->
     R1=file:make_dir(MainLogDir),
     R2=file:make_dir(ProviderLogDir),
+    case filelib:is_file(LogFilePath) of
+	false->
+	    file:write_file(LogFilePath," ");
+	true->
+	    ok
+    end,
     R3=create_logger(LogFilePath),
     {ok,[R1,R2,R3]}.
 
